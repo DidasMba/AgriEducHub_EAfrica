24291fad8ae2ed598980aefda24e4142d98ed855
DidasMba <daniel.mbarushi@gmail.com>
2023-12-28 18:55:00 +0300

    add background color in AppCss

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
-import Header from './Header'
+import Header from './components/Header'
 function App() {
   return (
     <div className="App">
