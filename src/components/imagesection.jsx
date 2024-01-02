e937953597ae5a3b65ed192618ad1f289258d226
DidasMba <daniel.mbarushi@gmail.com>
2024-01-02 16:35:30 +0300

    add imagesection

---
+++ src/components/imagesection.jsx
@@ -0,0 +1,14 @@
+// ImageSection.jsx
+
+import React from 'react';
+
+function ImageSection() {
+  return (
+    <section>
+      {/* Votre code pour l'image va ici */}
+      <img src="chemin/vers/votre/image.jpg" alt="Description de l'image" />
+    </section>
+  );
+}
+
+export default ImageSection;
