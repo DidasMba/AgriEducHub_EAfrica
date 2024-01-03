e880dc404f60c0e3f7bb9b804000dbf1ca74c295
DidasMba <daniel.mbarushi@gmail.com>
2024-01-03 21:12:22 +0300

    imageadd

--- src/components/imagesection.jsx
+++ src/components/imagesection.jsx
@@ -6,7 +6,8 @@ function ImageSection() {
   return (
     <section>
       {/* Votre code pour l'image va ici */}
-      <img src="chemin/vers/votre/image.jpg" alt="Description de l'image" />
+      {/*<img src="chemin/vers/votre/image.jpg" alt="Description de l'image" />*/}
+      <img src="imgs/agripic.jpg" class="img-fluid" alt="Responsive image"></img>
     </section>
   );
 }
