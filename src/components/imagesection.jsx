6f5a9e68d456729f09d271217f7daac993df1b4e
DidasMba <daniel.mbarushi@gmail.com>
2024-01-04 13:33:19 +0300

    edit imag

--- src/components/imagesection.jsx
+++ src/components/imagesection.jsx
@@ -6,7 +6,9 @@ function ImageSection() {
   return (
     <section>
       {/* Votre code pour l'image va ici */}
-      <img src="imgs/agripic.jpg" alt="Description de l'image" />
+      <img src="example.jpg" alt="Image of a cat" />
+
+
 
     </section>
   );
