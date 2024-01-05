88e94906fa7123331976d8600d3b59492e5e8b45
DidasMba <daniel.mbarushi@gmail.com>
2024-01-05 19:37:42 +0300

    srcSet changed

--- src/components/imagesection.jsx
+++ src/components/imagesection.jsx
@@ -21,7 +21,7 @@ function ImageSection() {
         className="wp-block-cover__image-background wp-image-2734 lazyloaded"
         src="/agripic.jpg"
         sizes="(max-width: 2560px) 100vw, 2560px"
-        srcset="/agripic.jpg 2560w, /agripic-300x200.jpg 300w, /agripic-1024x683.jpg 1024w, /agripic-768x512.jpg 768w, /agripic-1536x1024.jpg 1536w, /agripic-2048x1365.jpg 2048w, /agripic-18x12.jpg 18w"
+        srcSet="/agripic.jpg 2560w, /agripic-300x200.jpg 300w, /agripic-1024x683.jpg 1024w, /agripic-768x512.jpg 768w, /agripic-1536x1024.jpg 1536w, /agripic-2048x1365.jpg 2048w, /agripic-18x12.jpg 18w"
       />
 
     </section>
