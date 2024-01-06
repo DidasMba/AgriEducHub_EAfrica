45cfdbe5bdf713b666a49e8bf22b5f2f474e4681
DidasMba <daniel.mbarushi@gmail.com>
2024-01-06 16:14:10 +0300

    edit image

--- src/components/imagesection.jsx
+++ src/components/imagesection.jsx
@@ -9,15 +9,15 @@ function ImageSection() {
       <img
   fetchpriority="high"
   decoding="async"
-  width="2560"
-  height="1707"
+  width="100%"
+  height="100vh"
   alt=""
   style={{ objectPosition: '50% 36%' }}
   data-object-fit="cover"
   data-object-position="50% 36%"
   data-srcset="https://example.com/simple-image.jpg 2560w"
   data-src="https://example.com/simple-image.jpg"
-  data-sizes="(max-width: 2560px) 100vw, 2560px"
+  data-sizes="(max-width: 2000px) 100vw, 2560px"
   class="wp-block-cover__image-background wp-image-2734 lazyloaded"
   src=""
   sizes="(max-width: 2560px) 100vw, 2560px"
